9f35e45ed8c6c944443f901e5b1d35f6aad9a6e7
fnyisenge <f.nyisenge@alustudent.com>
2026-02-27 01:59:35 +0000

    New commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,45 +1,29 @@
 #!/usr/bin/python3
-"""
-Gather TODO list progress for a given employee using a REST API.
-"""
+"""Module"""
 
-import sys
 import requests
+import sys
 
 
-if __name__ == "__main__":
-    # Get the employee ID from command-line arguments
-    employee_id = int(sys.argv[1])
+"""Module"""
 
-    # URLs for user info and todos
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
-        employee_id
-    )
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/".format(
-        employee_id
-    )
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(user_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(user_id)
 
-    # Fetch data from API
-    user_info = requests.get(user_url).json()
-    todos_info = requests.get(todos_url).json()
+    user_info = requests.request('GET', user_url).json()
+    todos_info = requests.request('GET', todos_url).json()
 
-    # Extract employee name safely
-    employee_name = user_info.get("name")
-
-    # Filter completed tasks using .get() for safety
-    task_completed = list(filter(lambda task: task.get("completed"), todos_info))
-
-    # Count tasks
+    employee_name = user_info["name"]
+    task_completed = list(filter(lambda obj:
+                                 (obj["completed"] is True), todos_info))
     number_of_done_tasks = len(task_completed)
     total_number_of_tasks = len(todos_info)
 
-    # Print the summary line exactly as ALU expects
-    print(
-        "Employee {} is done with tasks({}/{}):".format(
-            employee_name, number_of_done_tasks, total_number_of_tasks
-        )
-    )
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_name, number_of_done_tasks, total_number_of_tasks))
 
-    # Print each completed task correctly formatted: 1 tab + 1 space
-    for task in task_completed:
-        print("\t " + task.get("title"))
+    [print("\t " + task["title"]) for task in task_completed]
