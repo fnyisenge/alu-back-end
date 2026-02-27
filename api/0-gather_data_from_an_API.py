187aa65a4f6c20e0885714307a5cb568e0a99eca
fnyisenge <f.nyisenge@alustudent.com>
2026-02-27 01:48:57 +0000

    new commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,8 +3,8 @@
 Gather TODO list progress for a given employee using a REST API.
 """
 
-import requests
 import sys
+import requests
 
 if __name__ == '__main__':
     user_id = int(sys.argv[1])
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     user_info = requests.get(user_url).json()
     todos_info = requests.get(todos_url).json()
 
-    employee_name = user_info["name"]
-    task_completed = list(filter(lambda obj: obj["completed"], todos_info))
+    employee_name = user_info.get("name")
+    task_completed = list(filter(lambda obj: obj.get("completed"), todos_info))
     number_of_done_tasks = len(task_completed)
     total_number_of_tasks = len(todos_info)
 
@@ -24,4 +24,4 @@ if __name__ == '__main__':
     ))
 
     for task in task_completed:
-        print("\t " + task["title"])
+        print("\t " + task.get("title"))
