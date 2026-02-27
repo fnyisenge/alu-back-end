51363adb82b77dbe39095c39bf95701ce7772418
fnyisenge <f.nyisenge@alustudent.com>
2026-02-27 01:30:27 +0000

    New commit

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Gather TODO list progress for a given employee using a REST API.
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    user_id = int(sys.argv[1])
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/".format(user_id)
+
+    user_info = requests.get(user_url).json()
+    todos_info = requests.get(todos_url).json()
+
+    employee_name = user_info["name"]
+    task_completed = list(filter(lambda obj: obj["completed"], todos_info))
+    number_of_done_tasks = len(task_completed)
+    total_number_of_tasks = len(todos_info)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, number_of_done_tasks, total_number_of_tasks
+    ))
+
+    for task in task_completed:
+        print("\t " + task["title"])
